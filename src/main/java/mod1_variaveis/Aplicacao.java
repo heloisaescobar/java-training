49170f2fb246373f71529bf7e49f7df4d6abd8ab
heloisaescobar <heloisa.escob@gmail.com>
2020-06-21 19:41:43 -0300

    criacao branch treinamento.

---
+++ src/main/java/mod1_variaveis/Aplicacao.java
@@ -0,0 +1,34 @@
+package mod1_variaveis;
+
+public class Aplicacao {
+
+    public static void main(String[] args) {
+
+        //int numero;
+        //numero = 50;
+
+        //int numero = 50;
+        //int numero2 = numero + 5;
+
+        //numero = numero - 1;
+        //numero -= 1;
+        // numero--;
+
+        //numero = numero * 2;
+        //numero *= 2;
+
+        //System.out.println(numero);
+        //System.out.println(numero2);
+
+        //boolean b = true;
+        //System.out.println(b);
+
+        //long i = 10L + 5;
+        //double d = 5 * 2.0;
+        //System.out.println(i);
+        //System.out.println(d);
+
+        double i = 0.0 / 0;
+        System.out.println(i);
+    }
+}
